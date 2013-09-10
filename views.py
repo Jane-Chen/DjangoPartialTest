97a80b92ddbd90c38bb0e959edcd5cf4dba9f890
Jane Chen <finnbubble@hotmail.com>
2013-09-10 18:01:46 +0800

    Initial commition.

---
+++ views.py
@@ -0,0 +1,37 @@
+# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.views import generic
+from polls.models import Poll, Choice
+
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question for p in latest_poll_list])
+    return HttpResponse(output)
+
+class DetailView(generic.DetailView):
+	model = Poll
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Poll
+	template_name = 'polls/results.html'
+
+def vote(request, poll_id):
+	p = get_object_or_404(Poll, pk=poll_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Rediaplay the poll voting form.
+		return render(request, 'polls/detail.html', {
+			'poll': p,
+			'error_message': "You didn't select a choice.",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		# Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
